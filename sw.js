6793c0fce70877032f45dd73303d4ac9a7aa766e
Safwat Ahmed <safwatahmed94@gmail.com>
2021-07-10 03:44:29 +0600

    second commit

---
+++ sw.js
@@ -0,0 +1,29 @@
+const version = "0.0.02";
+const cacheName = `dtd-${version}`;
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        `/`,
+        `/index.html`,
+        `/assets/css/main.min.css`,
+        `/assets/js/basic.min.js`
+      ])
+          .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
